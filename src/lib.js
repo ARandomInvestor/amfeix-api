10bf083db9ecf3f6cf5a7d0967b3cf34538ab941
ARandomInvestor <59703509+ARandomInvestor@users.noreply.github.com>
2020-06-16 20:13:37 +0200

    Changed exports

--- src/lib.js
+++ src/lib.js
@@ -1,24 +1,25 @@
 "use strict";
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
 const CacheProvider = require('./CacheProvider');
-exports.CacheProvider = CacheProvider;
-
 const StorageContract = require('./amfeix/StorageContract');
-exports.StorageContract = StorageContract;
 
 const InvestorAccount = require('./amfeix/InvestorAccount');
-exports.InvestorAccount = InvestorAccount;
 
 const BitcoinProvider = require('./bitcoin/BitcoinProvider');
-exports.BitcoinProvider = BitcoinProvider;
 const FullNodeBitcoinProvider = require('./bitcoin/FullNodeBitcoinProvider');
-exports.FullNodeBitcoinProvider = FullNodeBitcoinProvider;
 const BlockchainComBitcoinProvider = require('./bitcoin/BlockchainComBitcoinProvider');
-exports.BlockchainComBitcoinProvider = BlockchainComBitcoinProvider;
 const BitcoinUnitConverter = require('./bitcoin/BitcoinUnitConverter');
-exports.BitcoinUnitConverter = BitcoinUnitConverter;
 
 const EthereumUnitConverter = require('./ethereum/EthereumUnitConverter');
-exports.EthereumUnitConverter = EthereumUnitConverter;
+
+
+module.exports = {
+    CacheProvider: CacheProvider,
+    StorageContract: StorageContract,
+    InvestorAccount: InvestorAccount,
+    BitcoinProvider: BitcoinProvider,
+    FullNodeBitcoinProvider: FullNodeBitcoinProvider,
+    BlockchainComBitcoinProvider: BlockchainComBitcoinProvider,
+    BitcoinUnitConverter: BitcoinUnitConverter,
+    EthereumUnitConverter: EthereumUnitConverter
+};
